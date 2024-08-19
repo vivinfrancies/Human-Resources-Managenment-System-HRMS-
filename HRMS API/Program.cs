3be8cc358d60c7d9215b63d421df62846601fdcf
vivinfrancies <vivinfrancies@gmail.com>
2024-08-19 10:54:37 +0530

    updated for local host

--- HRMS API/Program.cs
+++ HRMS API/Program.cs
@@ -3,33 +3,36 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Ensure correct configuration loading
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var connectionString = builder.Configuration.GetConnectionString("AppDbConnection");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("The connection string 'AppDbConnection' is not configured.");
+}
+
+// Add services to the container
+builder.Services.AddDbContext<ModelContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-/*builder.Services.AddDbContext<Modelcontext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));*/
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddCors();
 
-builder.Services.AddDbContext<ModelContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbConnection")));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
 app.UseCors(o => o.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
